11254610810fcc37cb63b7c76e7ed4174bda8684
ArkaPrabhaChowdhury <92904175+ArkaPrabhaChowdhury@users.noreply.github.com>
2022-08-19 00:06:45 +0530

    Delete cart.js

--- cart.js
+++
@@ -1,7 +0,0 @@
-var close = document.getElementsByClassName("coupon");
-var i;
-for(i=0;i<close.length;i++){
-    close[i].addEventListener("click",function(){
-        this.parentElement.style.display = 'none';
-    });
-}
